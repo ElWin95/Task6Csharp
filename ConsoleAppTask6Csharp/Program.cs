afd6f8d3fbb3f69ba95cfdccc02b06cd46846487
ElWin95 <elvinma@code.edu.az>
2023-03-22 16:43:00 +0400

    'task1'

--- ConsoleAppTask6Csharp/Program.cs
+++ ConsoleAppTask6Csharp/Program.cs
@@ -5,32 +5,41 @@
         static void Main(string[] args)
         {
             #region Task1
-            Console.Write("C dəyərini daxil edin: ");
-            int c = int.Parse(Console.ReadLine());
+        //L1:
+        //    Console.Write("C deyerini daxil edin: ");
+        //    string strC = Console.ReadLine();
+        //    if (!int.TryParse(strC, out int c))
+        //    {
+        //        goto L1;
+        //    }
+        //L2:
+        //    Console.Write("Massivin uzunluğunu daxil edin: ");
+        //    string strN = Console.ReadLine();
+        //    if (!int.TryParse(strN, out int n))
+        //    {
+        //        goto L2;
+        //    }
+        //    int[] a = new int[n];
 
-            Console.Write("Massivin uzunluğunu daxil edin: ");
-            int n = int.Parse(Console.ReadLine());
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        Console.Write("a[" + (i+1) + "] deyerini daxil edin: ");
+        //        a[i] = int.Parse(Console.ReadLine());
+        //    }
 
-            int[] a = new int[n];
+        //    Console.Write("Massivin uyğunluğu: ");
 
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("a[" + i + "] dəyərini daxil edin: ");
-                a[i] = int.Parse(Console.ReadLine());
-            }
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        if (Math.Abs(a[i]) < c)
+        //        {
+        //            Console.Write(a[i] + " ");
+        //        }
+        //    }
 
-            Console.Write("Massivin uyğunluğu: ");
-
-            for (int i = 0; i < n; i++)
-            {
-                if (Math.Abs(a[i]) < c)
-                {
-                    Console.Write(a[i] + " ");
-                }
-            }
-
-            Console.ReadKey();
+        //    Console.ReadKey();
             #endregion
+
         }
     }
 }
